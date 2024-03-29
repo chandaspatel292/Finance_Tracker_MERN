f872b9613b8fcf047cae61b6b9cbea3ae19043ed
Chandas Patel C I <83898512+chandaspatel292@users.noreply.github.com>
2024-03-30 04:53:31 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,11 +9,17 @@ require("dotenv").config();
 const PORT = process.env.PORT;
 
 //middlewares
-app.use(express.json());
+//app.use(express.json());
 /*app.use(cors({
   origin: "https://finance-tracker-mern-449s.vercel.app"
 }));*/
-
+app.use((req, res, next) => {
+    // Set CORS headers based on request conditions
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    // ... other headers
+    next();
+});
 // Configure CORS
 app.use(cors({
   origin: "*", // Update this to the origin(s) you want to allow
